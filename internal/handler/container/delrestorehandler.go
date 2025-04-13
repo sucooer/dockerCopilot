0c0d3570e97dac146172c672e5943efd1f8311ba
onlyLTY <46625336+onlyLTY@users.noreply.github.com>
2025-04-13 17:58:30 +0800

    fea: compose备份支持

--- internal/handler/container/delrestorehandler.go
+++ internal/handler/container/delrestorehandler.go
@@ -11,7 +11,7 @@ import (
 
 func DelRestoreHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var req types.ContainerRestoreReq
+		var req types.DelContainerBackupReq
 		if err := httpx.Parse(r, &req); err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
 			return
