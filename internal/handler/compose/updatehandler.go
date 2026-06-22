bffd49169b6baf1ab58e34e847b927216490eb9d
sucooer <cocooer@mail.ru>
2026-06-22 14:17:46 +0000

    fix: v2.3.1 修复compose编辑保存报错EOF

--- internal/handler/compose/updatehandler.go
+++ internal/handler/compose/updatehandler.go
@@ -1,6 +1,8 @@
 package compose
 
 import (
+	"bytes"
+	"io"
 	"net/http"
 
 	"github.com/onlyLTY/dockerCopilot/internal/logic/compose"
@@ -12,11 +14,16 @@ import (
 
 func UpdateHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		bodyBytes, _ := io.ReadAll(r.Body)
+		r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
 		var req types.ComposeNameReq
 		if err := httpx.Parse(r, &req); err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
 			return
 		}
+
+		r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
 
 		var body types.ComposeUpdateReq
 		if err := httpx.Parse(r, &body); err != nil {
